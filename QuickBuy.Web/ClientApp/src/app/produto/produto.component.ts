62c6a53eb5d003453bfeb7f02fe3ed1692438d4c
jeln666 <je.luis.nascimento@hotmail.com>
2019-09-06 13:34:24 -0300

    Implementando template cadastro de produto

--- QuickBuy.Web/ClientApp/src/app/produto/produto.component.ts
+++ QuickBuy.Web/ClientApp/src/app/produto/produto.component.ts
@@ -1,14 +1,33 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Produto } from "../modelo/produto";
+import { ProdutoServico } from "../servicos/produto/produto.servico";
 
 @Component({
   selector: "app-produto",
   templateUrl: "produto.component.html",
   styleUrls: ["produto.component.css"]
 })
-export class ProdutoComponent {
-  nome: string;
+export class ProdutoComponent implements OnInit {
 
-  getNome(): string {
-    return "Samsung";
+  public produto: Produto;
+
+  ngOnInit(): void {
+    this.produto = new Produto();
+  }
+
+  constructor(private produtoServico: ProdutoServico) {
+
+  }
+
+  public cadastrar(): void {
+    this.produtoServico.cadastrar(this.produto)
+      .subscribe(
+        produto => {
+          console.log(produto);
+        },
+        err => {
+          console.log(err.error);
+        }
+      );
   }
 }
